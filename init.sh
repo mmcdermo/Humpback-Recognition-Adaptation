754cfb4a394fefee6f708084d5c60e972bd394e4
Morgan <morganmcdermott@gmail.com>
2016-03-29 15:15:44 -0700

    Initial

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export IMG_DIR="/home/mmcdermo/clients/cheeseman/deep/deepsense-whales/orig"
+export IMG_DIR_25="/home/mmcdermo/clients/cheeseman/deep/deepsense-whales/orig25"
+
+export PYTHONPATH="${PYTHONPATH}:."
